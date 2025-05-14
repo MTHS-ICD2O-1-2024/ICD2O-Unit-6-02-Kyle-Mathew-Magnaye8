b585450dc74debfecd37ddd2c14625cb5623ddde
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-05-14 23:33:08 +0000

    updated code 1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Kyle Matthew Magnaye
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kyle Matthew Magnaye
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+function updateCookieCount() {
+  // Example functionality for updating the cookie count
+  const resultOutput = document.getElementById("result-output");
+
+  let currentCount = parseInt(localStorage.getItem("cookieCount")) || 0;
+  currentCount++;
+  localStorage.setItem("cookieCount", currentCount);
+
+  resultOutput.textContent = `You have clicked ${currentCount} times`;
+}
